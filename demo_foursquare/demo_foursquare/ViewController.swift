b2a6b071010aff6ef8f452e875a055f4c652bf7c
boss tawee <bossnew1@gmail.com>
2019-11-21 12:38:47 +0700

    commit test

--- demo_foursquare/demo_foursquare/ViewController.swift
+++ demo_foursquare/demo_foursquare/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ddddd")
+        print("ddddssss")
         // Do any additional setup after loading the view.
     }
 
